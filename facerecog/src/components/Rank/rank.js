3ca5787445ba78fccd4a31131a0546becb73f1fd
anurag kanungo <anuragkanungo1818@gmail.com>
2022-08-13 23:18:06 +0530

    add

---
+++ facerecog/src/components/Rank/rank.js
@@ -0,0 +1,16 @@
+import React from "react";
+const Rank = () => {
+    return(
+        <div>
+            <div className="white f3">
+            {"anurag,.... rank is 1"}
+
+            </div>
+            <div className="white f1">
+            {"anurag,.... rank is 1"}
+
+            </div>
+        </div>
+    )
+}
+export default Rank;
