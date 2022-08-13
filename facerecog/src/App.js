9d1d614d5323347a3960fcf70bef31aa6e01924b
anurag kanungo <anuragkanungo1818@gmail.com>
2022-08-13 23:08:08 +0530

    rank

--- facerecog/src/App.js
+++ facerecog/src/App.js
@@ -1,18 +1,21 @@
 import React, {Component} from 'react';
 import Navigation  from './components/navigation/navigation';
 import Logo from './components/LOGO/Logo';
+import ImageLinkForm from './components/imageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/rank'
 import './App.css';
 
 class App extends Component {
-  render(){
+  render() {
     return(
       <div className="App">
       
        <Navigation />
        <Logo />
-      {/* 
+       <Rank />
+      
       <ImageLinkForm />
-      <facerecognition /> */}
+     
 
       </div>
     );
